7833e705d4f96232f170a40e5918378447ba18f9
allgenesconsidered@gmail.com <conklinlab@LAXECD07015.local>
2018-02-06 16:53:09 -0800

    Changed additional paramaters around get_chr_tables.sh, specfically how it checks for bcftools versions, and how it calls the python script. Confirmed working with multisample BEST1 vcfs.

--- preprocessing/generate_gens_dfs/get_chr_tables.sh
+++ preprocessing/generate_gens_dfs/get_chr_tables.sh
@@ -1,6 +1,7 @@
 # get chr tables
 # required that you have bcftools installed, at least version 1.5
 
+
 bcf_fn=$1
 chrom=$2
 outdir=$3
@@ -15,18 +16,23 @@ echo $1 $2 $3
 version=`bcftools -v | head -1 | cut -d ' ' -f2`
 
 req_version=1.5
-
-if [ "$version" == "$req_version" ]; then
+# Checks to see if the version is greater than or equal to 1.5.
+if [ "$(printf '%s\n' "$req_version" "$version" | sort | head -n1)" = "$req_version" ]; then
 	echo "bcftools version $version, running"
 else
-	echo "Error: bcftools must be 1.5. Current version: $version"
+	echo "Error: bcftools must be >=1.5. Current version: $version"
 	exit 1
 fi
 
 sample_list=`bcftools query -l $bcf_fn`
 
-bcftools norm -m - ${bcf_fn} | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%TGT]\n' --samples $sample_list > $outdir/${chrom}_prechrtable.txt
+# Removed --samples, it would not let me run multivariant vcfs
+bcftools norm -m - ${bcf_fn} | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%TGT]\n'  > $outdir/${chrom}_prechrtable.txt
 
-python fix_chr_tables.py $outdir/${chrom}_prechrtable.txt $chrom $outdir
+# Added $(dirname "$0"), so that is the script is called from another dir, this script can be accessed
+# Let me know what you think about this. Personally I like to run scripts from the directory where all my files are, or even
+# from a higher directory that contains both my scripts and my input/output files. I got the sample output whether
+# I ran the scrip from 'generate_gens_dfs' or not.
+python $(dirname "$0")/fix_chr_tables.py $outdir/${chrom}_prechrtable.txt $chrom $outdir
 
 rm $outdir/${chrom}_prechrtable.txt
