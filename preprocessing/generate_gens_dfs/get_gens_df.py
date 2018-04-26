2e8df02b845bc8ec28203c9dd817d4c0376556a6
kathleen <keoughkath@gmail.com>
2018-04-25 22:29:58 -0700

    making additions/fixes to feature to design personalized non-allele-specific sgRNA design

--- preprocessing/generate_gens_dfs/get_gens_df.py
+++ preprocessing/generate_gens_dfs/get_gens_df.py
@@ -2,14 +2,14 @@
 # Rewriting `get_chr_tables.sh` in python
 # -*- coding: utf-8 -*-
 """
-get_chr_tables.py generates a table (tsv file) listing all variants in a defined interval for a specified 
+get_gens_dfs.py generates a table (tsv file) listing all variants in a defined interval for a specified 
 individual (based on input VCF file). This basically reformats genotypes from VCF for easier 
 processing later when designing sgRNAs.
 Written in Python v 3.6.1.
 Kathleen Keough et al 2017-2018.
 
 Usage:
-	get_chr_tables.py <vcf_file> <locus> <out> [-f] [--bed] [--chrom]
+	get_gens_dfs.py <vcf_file> <locus> <out> [-f] [--bed] [--chrom]
 
 Arguments:
 	vcf_file           The sample vcf file, separated by chromosome. BCF also supported. 
@@ -28,12 +28,14 @@ import pandas as pd
 from docopt import docopt
 import subprocess, os, sys
 import regex as re
+from io import StringIO
 
 __version__='0.0.2'
 
 REQUIRED_BCFTOOLS_VER = 1.5
 
 def norm_chr(chrom_str, vcf_chrom):
+	chrom_str = str(chrom_str)
 	if not vcf_chrom:
 		return chrom_str.replace('chr','')
 	elif vcf_chrom:
@@ -100,70 +102,122 @@ def main(args):
 	# if input is a BED file, run recursively
 	if args['--bed']:
 		bed_file = args['<locus>']
+		out = args['<out>']
 		print(f'Analyzing BED file {bed_file}')
 		bed_df = pd.read_csv(bed_file, sep='\t', header=0, names=['chr','start','stop','locus'])
-		hdf_out = pd.HDFStore(args['<out>'] + '.h5')
-		for index, row in bed_df.iterrows():
+		vcf_chrom = str(subprocess.Popen(f'bcftools view -H {vcf_in} | cut -f1 | head -1', shell=True, 
+			stdout=subprocess.PIPE).communicate()[0])
+		# See if chrom contains chr
+		if vcf_chrom.startswith('chr'):
+			chrstart = True
+		else:
+			chrstart = False
+
+		# removes or adds "chr" based on analyzed VCF
+		bed_df['chr'] = [ norm_chr(chrom, chrstart) for chrom in bed_df['chr'].tolist() ]
+
+		# write to temp file
+		bed_df.to_csv(f'{out}_temp.bed', index=False, sep='\t', header=False)
+
+		# gets genotypes at locus of interest, excluding those where 1+ samples missing a genotype call
+
+		# if option -f specified, indicating to keep homozygous variants, do so
+		if args['-f']:
+			bcl_v=f"bcftools view -g ^miss -R {out}_temp.bed {args['<vcf_file>']}"
+		else:
+			bcl_v=f"bcftools view -g ^miss -g het -R {out}_temp.bed {args['<vcf_file>']}"
+		
+		# Pipe for bcftools
+		bcl_view = subprocess.Popen(bcl_v,shell=True, stdout=subprocess.PIPE)
+		# splits multiallelic sites into multiple lines
+		bcl_norm = subprocess.Popen("bcftools norm -m -",shell=True, stdin=bcl_view.stdout, stdout=subprocess.PIPE)
+		bcl_query = subprocess.Popen("bcftools query -f '%CHROM\t%POS\t%REF\t%ALT{0}\n'",shell=True,
+		 stdin=bcl_norm.stdout, stdout=subprocess.PIPE)
+		bcl_query.wait() # Don't do anything else untill bcl_query is done running.
+
+		# output  
+		raw_dat = pd.read_csv(StringIO(bcl_query.communicate()[0].decode("utf-8")), sep='\t')
+		raw_dat.columns = ['chrom','pos','ref','alt']
+
+		# save to HDF
+		raw_dat.to_hdf(f'{out}_gens.h5','all', data_columns=True)
+		os.remove(f'{out}_temp.bed')
+		print('finished')
+
+
+		# hdf_out = pd.HDFStore(args['<out>'] + '.h5')
+		# for index, row in bed_df.iterrows():
 			
-			# check whether chromosome in VCF file includes "chr" in chromosome
-			vcf_chrom = str(subprocess.Popen(f'gzcat {vcf_in} | tail -1 | cut -f1', shell=True))
+		# 	# check whether chromosome in VCF file includes "chr" in chromosome
+		# 	vcf_chrom = str(subprocess.Popen(f'gzcat {vcf_in} | tail -1 | cut -f1', shell=True))
 
-			if vcf_chrom.startswith('chr'):
-				chrstart = True
-			else:
-				chrstart = False
+		# 	if vcf_chrom.startswith('chr'):
+		# 		chrstart = True
+		# 	else:
+		# 		chrstart = False
 
-			# See if chrom contains chr
-			chrom = str(row['chr'])
-			start = row['start']
-			stop = row['stop']
+		# 	# See if chrom contains chr
+		# 	chrom = str(row['chr'])
+		# 	start = row['start']
+		# 	stop = row['stop']
 
-			# removes or adds "chr" based on analyzed VCF
-			chr_name = norm_chr(chrom, chrstart)
+		# 	# removes or adds "chr" based on analyzed VCF
+		# 	chr_name = norm_chr(chrom, chrstart)
 
-			bcl_v=f"bcftools view -g ^miss -r {chr_name}:{str(start)}-{str(stop)} {args['<vcf_file>']}"
+		# 	# gets genotypes at locus of interest, excluding those where 1+ samples missing a genotype call
+		# 	# if option -f specified, indicating to keep homozygous variants, do so
+		# 	if args['-f']:
+		# 		bcl_v=f"bcftools view -g ^miss -r {chr_name}:{str(start)}-{str(stop)} {args['<vcf_file>']}"
+		# 	else:
+		# 		bcl_v=f"bcftools view -g ^miss -g het -r {chr_name}:{str(start)}-{str(stop)} {args['<vcf_file>']}"
 			
-			# Pipe for bcftools
-			bcl_view = subprocess.Popen(bcl_v,shell=True, stdout=subprocess.PIPE)
-			bcl_norm = subprocess.Popen("bcftools norm -m -",shell=True, stdin=bcl_view.stdout, stdout=subprocess.PIPE)
-			bcl_query = subprocess.Popen("bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%TGT]\n'",shell=True,
-			 stdin=bcl_norm.stdout, stdout=subprocess.PIPE)
-			bcl_query.wait() # Don't do anything else untill bcl_query is done running.
+		# 	# Pipe for bcftools
+		# 	bcl_view = subprocess.Popen(bcl_v,shell=True, stdout=subprocess.PIPE)
+		# 	# splits multiallelic sites into multiple lines
+		# 	bcl_norm = subprocess.Popen("bcftools norm -m -",shell=True, stdin=bcl_view.stdout, stdout=subprocess.PIPE)
+		# 	bcl_query = subprocess.Popen("bcftools query -f '%CHROM\t%POS\t%REF\t%ALT{0}\n'",shell=True,
+		# 	 stdin=bcl_norm.stdout, stdout=subprocess.PIPE)
+		# 	bcl_query.wait() # Don't do anything else untill bcl_query is done running.
 
-			# output  
-			raw_dat = bcl_query.communicate()[0].decode("utf-8")
+		# 	# output  
+		# 	raw_dat = StringIO(bcl_query.communicate()[0].decode("utf-8"))
 
-			temp_file_name=f"{os.path.dirname(args['<out>'])}/{str(chrom)}_prechrtable.txt"
-			with open(temp_file_name, 'w') as f:
-				f.write(raw_dat)
-				f.close()
+		# 	# temp_file_name=f"{args['<out>']}{str(chrom)}_prechrtable.txt"
+		# 	# with open(temp_file_name, 'w') as f:
+		# 	# 	f.write(raw_dat)
+		# 	# 	f.close()
 
-			# Append fix_chr_tables.py
-			vars = pd.read_csv(temp_file_name, sep='\t', header=None, names=['chrom', 'pos', 'ref', 'alt', 'genotype'],
-				usecols=['chrom', 'pos', 'ref', 'alt', 'genotype'])
+		# 	# generate output format
+		# 	vars = pd.read_csv(raw_dat, sep='\t', header=None, names=['chrom', 'pos', 'ref', 'alt'],
+		# 		usecols=['chrom', 'pos', 'ref', 'alt'])
 
-			if vars.empty and args['-f']:
-				print('No variants in this region for this individual. Moving on.')
-				os.remove(temp_file_name)
-				continue
-			elif vars.empty and not args['-f']:
-				print('No heterozygous variants in this region for this individual. Moving on.')
-				os.remove(temp_file_name)
-				continue
+		# 	if vars.empty and args['-f']:
+		# 		print('No variants in this region for this individual. Moving on.')
+		# 		# os.remove(temp_file_name)
+		# 		continue
+		# 	elif vars.empty and not args['-f']:
+		# 		print('No heterozygous variants in this region for this individual. Moving on.')
+		# 		# os.remove(temp_file_name)
+		# 		continue
 
-			# this looks like it might be redundant now
-			if 'chr' in str(vars.chrom.iloc[0]):
-				vars['chrom'] = vars['chrom'].map(lambda x: norm_chr(x))
+		# 	# # this looks like it might be redundant now
+		# 	# if 'chr' in str(vars.chrom.iloc[0]):
+		# 	# 	vars['chrom'] = vars['chrom'].map(lambda x: norm_chr(x))
 
-			if args['-f']:
-				vars_fixed = vars.applymap(fix_multiallelics)
-			else:
-				vars_fixed = filter_hets(vars.applymap(fix_multiallelics))
+		# 	# I'm pretty sure this is redundant too with alt{0} part in bcftools command
+		# 	# if args['-f']:
+		# 	# 	vars_fixed = vars.applymap(fix_multiallelics)
+		# 	# this was made redundant with change to bcftools argument above
+		# 	# else:
+		# 	# 	vars_fixed = filter_hets(vars.applymap(fix_multiallelics))
 
-			locus_name = {row['locus']}
-			hdf_out.put(row['locus'],vars_fixed, format='t', data_columns=True, complib='blosc')
-			print(f'{locus_name} done.')
-			os.remove(temp_file_name)
+		# 	locus_name = row['locus']
+		# 	print(locus_name)
+		# 	hdf_out.put(fix_natural_language(locus_name), vars)
+		# 	# hdf_out.put(row['locus'],vars_fixed, format='t', data_columns=True, complib='blosc')
+		# 	print(f'{locus_name} done.')
+			# os.remove(temp_file_name)
+		# hdf_out.close()
 
 	elif args['<locus>'].endswith('.bed') or args['<locus>'].endswith('.BED'):
 		print('Must specify --bed if inputting a BED file. Exiting.')
