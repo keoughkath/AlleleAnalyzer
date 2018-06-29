c57e4c91cc43d4afba835bcbf2b181ae3b815d14
kathleen <keoughkath@gmail.com>
2018-06-29 11:19:32 -0700

    issue 28

--- preprocessing/find_pams_in_reference/pam_pos_genome.py
+++ preprocessing/find_pams_in_reference/pam_pos_genome.py
@@ -6,12 +6,15 @@ Written in Python v 3.6.1.
 Kathleen Keough et al 2017-2018.
 
 Usage:
-	pam_pos_genome.py <chrom> <fasta> <out>
+	pam_pos_genome.py <chrom> <fasta> <cas_list> <out>
 
 Arguments:
 	chrom 			Chromosome being analyzed.
 	fasta 			Fasta file for genome being analyzed.
+    cas_list        Comma separated (no spaces!) list of Cas varieties to evaluate, options below.
 	out 			Out prefix for returned files.
+
+Available Cas types = cpf1,SpCas9,SpCas9_VRER,SpCas9_EQR,SpCas9_VQR_1,SpCas9_VQR_2,StCas9,StCas9_2,SaCas9,SaCas9_KKH,nmCas9,cjCas9
 """
 
 import numpy as np
@@ -22,7 +25,20 @@ import regex
 import re
 from Bio import SeqIO
 
-__version__='0.0.2'
+__version__='0.0.3'
+
+# 3 and 5 prime cas lists
+
+# Get apsolute path for gen_targ_dfs.py, and edit it for cas_object.py
+ef_path = os.path.dirname(os.path.realpath(__file__))
+cas_obj_path = ef_path.replace('preprocessing/find_pams_in_reference','scripts/')
+sys.path.append(cas_obj_path)
+
+metadata_path = ef_path.replace('find_pams_in_reference','')
+sys.path.append(metadata_path)
+# Import cas_object
+import cas_object as cas_obj
+from get_metadata import add_metadata
 
 # get rid of annoying false positive Pandas error
 
@@ -101,9 +117,9 @@ def find_the_pams(seqio_object):
 
     return(pam_positions)
 
-def find_spec_pams(cas,python_string,orient='3prime'):
-	# orient specifies whether this is a 3prime PAM (e.g. Cas9, PAM seq 3' of sgRNA)
-	# or a 5prime PAM (e.g. cpf1, PAM 5' of sgRNA)
+def find_spec_pams(cas_obj,python_string,orient="3'"):
+    # orient specifies whether this is a 3prime PAM (e.g. Cas9, PAM seq 3' of sgRNA)
+    # or a 5prime PAM (e.g. cpf1, PAM 5' of sgRNA)
 
     # get sequence 
 
@@ -123,12 +139,12 @@ def find_spec_pams(cas,python_string,orient='3prime'):
             starts.add(pam.end()) 
         return(set(starts))
 
-    if orient == '3prime':
-    	for_starts = get_pam_fiveprime(tpp_for[cas],sequence)
-    	rev_starts = get_pam_threeprime(tpp_rev[cas+'_rev'],sequence)
-    elif orient == '5prime':
-    	for_starts = get_pam_threeprime(fpp_for[cas],sequence)
-    	rev_starts = get_pam_fiveprime(fpp_rev[cas+'_rev'],sequence)
+    if orient == "3'":
+        for_starts = get_pam_fiveprime(cas_obj.forwardPam_regex() ,sequence)
+        rev_starts = get_pam_threeprime(cas_obj.reversePam_regex(),sequence)
+    elif orient == "5'":
+        for_starts = get_pam_threeprime(cas_obj.forwardPam_regex(),sequence)
+        rev_starts = get_pam_fiveprime(cas_obj.reversePam_regex(),sequence)
 
     return(for_starts,rev_starts)
 
@@ -146,23 +162,17 @@ def main(args):
 
 	genome = Fasta(args['<fasta>'],as_raw=True)
 
-	cas_list = ['SpCas9','SpCas9_VRER','SpCas9_EQR','SpCas9_VQR_1',
-	'SpCas9_VQR_2','StCas9','StCas9_2','SaCas9','SaCas9_KKH','nmCas9','cjCas9']
+	cas_list = args['<cas_list'].split(',')
 
 	# get set of positions for each type of cas
 
 	for cas in cas_list:
-		for_starts, rev_starts = find_spec_pams(cas,str(genome[str(chrom)]))
+        current_cas = cas_obj.get_cas_enzyme(cas, os.path.join(cas_obj_path,'CAS_LIST.txt'))
+		for_starts, rev_starts = find_spec_pams(cas,str(genome[str(chrom)]), orient=current_cas.primeness)
 		savestr_for = f'{outprefix}chr'+str(chrom)+'_'+str(cas) + '_pam_sites_for.npy'
 		savestr_rev = f'{outprefix}chr'+str(chrom)+'_'+str(cas) + '_pam_sites_rev.npy'
 		np.save(savestr_for,list(for_starts))
 		np.save(savestr_rev,list(rev_starts))
-
-	# cpf1 is 5pp
-
-	for_starts, rev_starts = crisprtools.find_spec_pams('cpf1',str(hg19[str(chrom)]),orient='5prime')
-	np.save(f'{outprefix}chr'+str(chrom)+'_'+'cpf1_pam_sites_for.npy',for_starts)
-	np.save(f'{outprefix}chr'+str(chrom)+'_'+'cpf1_pam_sites_rev.npy',rev_starts)
 
 
 if __name__ == '__main__':
